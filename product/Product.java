c320b499b7f70103cd45440d0d45d65b9050c846
seoset <110677435+seoset@users.noreply.github.com>
2023-01-26 23:52:58 +0300

    Add files via upload

---
+++ product/Product.java
@@ -0,0 +1,42 @@
+package product;
+
+import java.util.Objects;
+
+public class Product {
+    private final String name;
+    private final int weight;
+
+    public Product(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return weight == product.weight && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, weight);
+    }
+}
